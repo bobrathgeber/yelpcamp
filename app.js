79ceef2a5b67175b99644e731aa2747e166661d1
Bob Rathgeber <bob.rathgeber@gmail.com>
2019-08-26 22:00:37 -0400

    Listen Update

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get("/about", function(req, res){
 console.log("Hello!");
 
 //Start listening for requests
-app.listen(process.env.PORT, function() {
-	console.log('Server listening on port 30000');
+app.listen(process.env.PORT, process.env.IP, function() {
+	console.log('Server listening on port '+ process.env.PORT);
 });
