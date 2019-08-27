b9312de63fe5e59a58c821d178e17d30dc3990a2
Bob Rathgeber <bob.rathgeber@gmail.com>
2019-08-26 20:53:00 -0400

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+console.log("Hello!");
+
+//Start listening for requests
+app.listen(30000, function() {
+	console.log('Server listening on port 30000');
+});
